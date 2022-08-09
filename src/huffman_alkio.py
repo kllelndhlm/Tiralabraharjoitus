60d523b29e306f5457464fadb6d140e2bdde5a69
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-08-10 01:26:06 +0300

    Huffmanin puun alkio

---
+++ src/huffman_alkio.py
@@ -0,0 +1,6 @@
+class Alkio:
+    def __init__(self, merkki, esiintymistiheys, vasen_lapsi = None, oikea_lapsi = None):
+        self.merkki = merkki
+        self.esiintymistiheys = esiintymistiheys
+        self.vasen_lapsi = vasen_lapsi
+        self.oikea_lapsi = oikea_lapsi
