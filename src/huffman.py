3844f4ecaee28fa6ba650438b7902d4f49a61857
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-08-10 14:24:13 +0300

    huffman

--- src/huffman.py
+++ src/huffman.py
@@ -1,24 +1,32 @@
+from huffman_alkio import Alkio
+
 class Huffman:
     def __init__(self, teksti):
         self.teksti = teksti
+        self.alkiot = []
+        self.sanakirja = {}
 
     def esiintymistiheys(self):
-        sanakirja = {}
-
         for i in self.teksti:
-            if i not in sanakirja:
-                sanakirja[i] = 1
+            if i not in self.sanakirja:
+                self.sanakirja[i] = 1
             else:
-                sanakirja[i] +=1
-
-        return sanakirja
+                self.sanakirja[i] +=1
+        return self.sanakirja
 
     def jarjestaminen(self):
         sanakirja = self.esiintymistiheys()
-
         jarjestetty_sanakirja = sorted(sanakirja.items(), key=lambda x: x[1])
-
         return jarjestetty_sanakirja
+
+    def luo_alkiot(self):
+        merkkiarvoparit = self.sanakirja
+        for key, value in merkkiarvoparit.items():
+            self.alkiot.append(Alkio(key, value))
+
+    def tiivistys(self):
+        self.esiintymistiheys()
+        self.luo_alkiot()
 
     def __str__(self):
         return f"{self.teksti}"
--- src/index.py
+++ src/index.py
@@ -1,6 +1,13 @@
+from huffman import Huffman
+
 def main():
-    luettu_teksti = input("HUFFMAN?")  #KOKEILUA
-    if luettu_teksti == "y":  #KOKEILUA
-        print("HUFFMAN!!!")  #KOKEILUA
+    avauskysymys = input("Tiivistetäänkö huffmanilla? (y/n)")  #KOKEILUA
+    if avauskysymys == "n":  #KOKEILUA
+        main()
+    if avauskysymys == "y":  #KOKEILUA
+        tiedostokysely = input("Mikä tiivistetään huffmanilla? (tiedostopolku)")  #KOKEILUA
+        with open(f"{tiedostokysely}", encoding="utf-8") as teksti:
+            huffman = Huffman(teksti.read())
+            Huffman.tiivistys(huffman)
 if __name__ == "__main__":
     main()
